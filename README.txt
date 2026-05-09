15569477d78901f94928fede7b5c26410e50aaca
edtrade3-beep <edtrade3@gmail.com>
2026-05-09 09:41:26 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,13 @@
+Athan & Quran App With Custom Logo
+
+Logo added:
+- Sidebar logo
+- Main app logo
+- Phone app icons
+- PWA home screen icon
+
+Deploy to Vercel:
+1. Upload this folder to Vercel
+2. Deploy
+3. Open on phone
+4. Add to Home Screen
---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "athan-quran-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
